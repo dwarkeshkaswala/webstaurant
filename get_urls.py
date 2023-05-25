c5e7b432f1ec40e941c0eb16272ada1865d65e01
Dwarkesh Kaswala <dwarkeshkaswala777@gmail.com>
2023-05-25 17:07:56 -0500

    updated

--- get_urls.py
+++ get_urls.py
@@ -7,23 +7,36 @@ def remove_dupes(lst):
     return lst
 
 def link_checker(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    prod_listing = soup.find("div", id="product_listing")
-    if prod_listing:
-        return True
-    else:
-        return False
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        prod_listing = soup.find("div", id="product_listing")
+        if prod_listing:
+            return True
+        else:
+            return False
+    except:
+        if "comhttps" in url:
+            url = url.replace("https://www.webstaurantstore.comh", "h")
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, "html.parser")
+            prod_listing = soup.find("div", id="product_listing")
+            if prod_listing:
+                return True
+            else:
+                return False
+        else:
+            print(f"Error with {url}")
     
 def get_links(url):
 
     try:
-        print(url)
+        # print(url)
         response = requests.get(url)
         soup = BeautifulSoup(response.content, "html.parser")
         main_div = soup.find("div", id="main")
         photo_grids = main_div.select('div[data-testid="Photo Grid Categories"]')
-        carousel = main_div.select('div[data-testid="enhanced-carousel-background"]')
+        carousels = main_div.select('div[data-testid="enhanced-carousel-background"]')
     except:
         print("Sleeping for 90 seconds..........")
         sleep(90)
@@ -31,16 +44,24 @@ def get_links(url):
         soup = BeautifulSoup(response.content, "html.parser")
         main_div = soup.find("div", id="main")
         photo_grids = main_div.select('div[data-testid="Photo Grid Categories"]')
-        carousel = main_div.select('div[data-testid="enhanced-carousel-background"]')
+        carousels = main_div.select('div[data-testid="enhanced-carousel-background"]')
 
     links = []
     for photo_grid in photo_grids:
-        links += ["https://www.webstaurantstore.com"+a["href"].strip() for a in photo_grid.select("a")]
+        for a in photo_grid.select("a"):
+            if not "https://www.webstaurantstore.com" in a["href"].strip():
+                links.append("https://www.webstaurantstore.com"+a["href"].strip())
+            else:
+                links.append(a["href"].strip())
     
-    for carousel in carousel:
-        links += ["https://www.webstaurantstore.com"+a["href"].strip() for a in carousel.select("a")]
-    
-    matchers = ['?filter','new/?category', 'specials.html?', '/search', 'vendor']
+    for carousel in carousels:
+        for a in carousel.select("a"):
+            if not "https://www.webstaurantstore.com" in a["href"].strip():
+                links.append("https://www.webstaurantstore.com"+a["href"].strip())
+            else:
+                links.append(a["href"].strip())
+                
+    matchers = ['feature','?filter','new/?category', 'specials.html?', '/search', 'vendor', url]
     cleaned = [s for s in links if not any(xs in s for xs in matchers)]
 
     cleaned = remove_dupes(cleaned)
@@ -50,11 +71,14 @@ def get_links(url):
     temp_true = []
 
     for link in cleaned:
+        
         status = link_checker(link)
         if status==True:
             true_links.append(link)
         else:
             false_links.append(link)
+        
+            
 
     return true_links, false_links
 
@@ -96,11 +120,15 @@ def driver(url, cat_name):
             json.dump(false_links, f, indent=4)
     
 
-def looper(next_traversed_file_no, cat_name):
+def looper(next_traversed_file_no, cat_name, org_url):
     while True:
-        files_dir = os.listdir(f'tests/{cat_name}/false_links')
-        temp = [int(i.split("_")[2].split(".")[0]) for i in files_dir]
-        max_file_no = max(temp)+1
+        try:
+            files_dir = os.listdir(f'tests/{cat_name}/false_links')
+            temp = [int(i.split("_")[2].split(".")[0]) for i in files_dir]
+            max_file_no = max(temp)+1
+        except:
+            print(f"{cat_name} | No further links found")
+            break
 
         if next_traversed_file_no == max_file_no:
             break
@@ -110,7 +138,9 @@ def looper(next_traversed_file_no, cat_name):
             
             m=1
             for link in false_links:
-                print(f"{m}/{len(false_links)} | false_links_{next_traversed_file_no}.json")
+                if link == org_url:
+                    continue
+                print(f"{m}/{len(false_links)} | false_links_{next_traversed_file_no}.json | {link}")
                 driver(link, cat_name)
                 m+=1
             next_traversed_file_no += 1
@@ -143,7 +173,7 @@ def merge_json(cat_name):
 def base_builder(url, cat_name):
     driver(url, cat_name)
     next_traversed_file_no = 1
-    looper(next_traversed_file_no, cat_name)
+    looper(next_traversed_file_no, cat_name, url)
     merge_json(cat_name)
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -221,8 +221,9 @@ def details_driver(cat_name, filename, continue_from):
 
     x = 1
     for link in links:
+        #GD = Getting Details
         if x >= continue_from:
-            print(f"{x}/{len(links)} | {link}")
+            print(f"GD | {x}/{len(links)} | {cat_name} > {filename} | {link}")
             details = get_details(link)
             all_details.append(details)
             
@@ -248,24 +249,53 @@ def get_total_pages(url):
 
 def main():
     base_url = "https://www.webstaurantstore.com"
-    url = "https://www.webstaurantstore.com/refrigeration-equipment.html"
-    cat_name = "refrigeration-equipment"
+    urls = [
+        "https://www.webstaurantstore.com/48429/commercial-fryers.html",
+        "https://www.webstaurantstore.com/51259/commercial-grills.html",
+        "https://www.webstaurantstore.com/14245/commercial-food-processors.html",
+        "https://www.webstaurantstore.com/14199/meat-slicers.html",
+        "https://www.webstaurantstore.com/14255/commercial-mixers.html",
+        "https://www.webstaurantstore.com/50445/outdoor-cooking-equipment.html",
+        "https://www.webstaurantstore.com/65959/smart-kitchen-appliances.html",
+        "https://www.webstaurantstore.com/shelving.html",
+        "https://www.webstaurantstore.com/57485/commercial-exhaust-hoods.html",
+        "https://www.webstaurantstore.com/50415/commercial-ice-equipment-and-supplies.html",
+        "https://www.webstaurantstore.com/plumbing-and-faucets.html",
+        "https://www.webstaurantstore.com/14251/commercial-blenders-food-blenders.html",
+        "https://www.webstaurantstore.com/55497/commercial-scales.html",
+        "https://www.webstaurantstore.com/14279/vacuum-packaging-machines.html",
+        "https://www.webstaurantstore.com/62457/food-packaging-machines.html",
+        "https://www.webstaurantstore.com/67563/candy-making-machines.html",
+    ]
+    url = "https://www.webstaurantstore.com/15037/commercial-restaurant-ranges.html"
+    cat_name = url.split("/")[-1].split(".")[0]
 
     base_builder(url, cat_name)
+
+    print("\nBase Build... Done\n")
 
     with open(f"tests/{cat_name}/true_links/merged_true_links.json", "r") as f:
         links = json.load(f)
     
-    for x in range(1, len(links)+1):
-        print(f"========================|LINK|========================")
-        print(links[x])
+    for x in range(1, len(links)):
         filename  = links[x].split("/")[-1].split(".")[0]
+        link_name = links[x]
+        print(f"|========================| {cat_name} |========================|")
+        print(f"{filename} | {link_name}")
+        print(f"|==========================================================================================|\n")
+    
         list_driver(1, cat_name, filename, links[x])
         details_driver(cat_name, filename, continue_from=1)
         # json2xlsx(filename)
     
-    # filename  = links[28].split("/")[-1].split(".")[0]
-    # details_driver(cat_name, filename, continue_from=2058)
+    # print(f"|========================| {cat_name} |========================|")
+    # print(links[124])
+    # print(f"|==============================================================================================|\n")
+
+    # filename  = links[124].split("/")[-1].split(".")[0]
+    # print(filename)
+    # # filename="_1"
+    # details_driver(cat_name, filename, continue_from=1)
             
     
 if __name__ == "__main__":
